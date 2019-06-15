366575038443e453eb80522b7b377d3008bdd740
kira3691 <kiran.k@accion.labs.com>
2019-06-15 22:33:51 +0530

    Debugging in node js and usage of yargs to use command line options

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,69 @@
-// const getNotes = require('./notes');
+const yargs=  require('yargs');
+const notesOperation = require('./notes');
 
-// console.log(getNotes('notes.txt'));
+//console.log(process.argv);
 
-const chalk = require('chalk');
+yargs.version("1.1.0");
+yargs.command({
+    command:'add',
+    describe:'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note Body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler:(argv) => {
+        notesOperation.addNote(argv.title, argv.body);
+    }
+});
 
-console.log(chalk.green.bold.inverse('hi'));
+yargs.command({
+    command:'remove',
+    describe:'Removes a note',
+    builder: {
+        title: {
+            describe: 'title',
+            demandOption: 'true',
+            type: "string"
+        }
+    },
+    handler:(argv) => {
+        notesOperation.removeNote(argv.title);
+    }
+});
+
+yargs.command({
+    command:'list',
+    describe:'Lists all note',
+    handler: () => {
+        notesOperation.listNotes();
+    }
+});
+
+yargs.command({
+    command:'read',
+    describe:'Reads a note',
+    builder: {
+        title: {
+            type: 'string',
+            description: 'Title of note to access',
+            demandOption: 'true'
+        }
+    },
+    handler: (argv) => {
+        notesOperation.readNote(argv.title);
+    }
+});
+
+yargs.parse();
+//console.log(yargs.argv);
+/**  your code did not work before cause you need to access yargs.argv at the end
+to tell node to do something with them (extremely lame)
+use yargs.parse() instead */
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,9 +1,84 @@
 const fs = require('fs');
+const chalk = require('chalk');
 
-const getNotes = (path) => {
-    const result = fs.readFileSync(path, 'utf8');
+const getNotes = () => {
+    try {
+        var result = fs.readFileSync('notes.json');
+        result = result.toString();
 
-    return result;
+        return JSON.parse(result);
+    } catch (e) {
+        return [];
+    }
 };
 
-module.exports = getNotes;
+const addNote = (title, body) => {
+    debugger
+    const data = getNotes();
+    const duplicateNotes = data.find(item => {
+        return item.title === title;
+    });
+
+    if (!duplicateNotes) {
+        data.push({
+            title: title,
+            body: body
+        })
+
+        saveNotes(data);
+        console.log(chalk.green('New note added'))
+    } else {
+        console.log(chalk.red('Note with same title already present'));
+    }
+}
+
+const listNotes = () => {
+    const data = getNotes();
+
+    console.log(chalk.green.inverse('Your notes are:'));
+    data.map(note => {
+        console.log(chalk.blue(note.title));
+    })
+}
+
+const readNote = (title) => {
+    const data = getNotes();
+
+    const note = data.find(note => {
+        return note.title === title;
+    });
+
+    if (note) {
+        console.log(chalk.inverse.green(note.title));
+        console.log(chalk.green(note.body), '\n');
+    } else {
+        console.log(chalk.bold.red('Note not found'));
+    }
+}
+
+const removeNote = (title) => {
+    const data = getNotes();
+
+    const updatedData = data.filter(item => {
+        return item.title !== title;
+    });
+
+    if (updatedData.length !== data.length) {
+        saveNotes(updatedData);
+        console.log(chalk.green('Notes updated'));
+    } else {
+        console.log(chalk.red(`Item with ${title} not present`));
+    }
+}
+
+const saveNotes = (data) => {
+    const dataStr = JSON.stringify(data);
+    fs.writeFileSync('notes.json', dataStr);
+}
+
+module.exports = {
+    addNote: addNote,
+    removeNote: removeNote,
+    listNotes: listNotes,
+    readNote: readNote
+};
